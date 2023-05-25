e6a2c36d5fcc5c1e56d6eae7d5adae6f1a35f055
apollo <baotrung06092003@gmail.com>
2023-05-25 22:02:41 +0700

    change link

--- src/pages/Home/Data/api.js
+++ src/pages/Home/Data/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchData() {
     const response = await axios({
-        url: 'https://0656-116-96-46-192.ngrok-free.app/getClasses',
+        url: 'https://1bb6-116-96-46-192.ngrok-free.app/getClasses',
         method: 'GET',
         headers: {
             "ngrok-skip-browser-warning": "true",
